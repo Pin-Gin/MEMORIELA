c27fc75a6a3ac7281dde9d86ce442f2463972591
Pin銀 <317489986+Pin-Gin@users.noreply.github.com>
2026-09-19 03:14:57 +0900

    Remove Development History from About header

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -8,7 +8,6 @@ export default function AboutPage() {
           <a className="active" href="/about">About</a>
           <a href="/technology">Technology</a>
           <a href="/gallery">Gallery</a>
-          <a href="/#history">Development History</a>
         </nav>
       </header>
 
