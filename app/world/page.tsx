68d5a5d3a43598a3127c19381fff209057292651
Pin銀 <317489986+Pin-Gin@users.noreply.github.com>
2026-09-19 04:28:22 +0900

    Remove News from desktop navigation

--- app/world/page.tsx
+++ app/world/page.tsx
@@ -7,7 +7,6 @@ export default function WorldPage() {
           <a href="/about">About</a>
           <a href="/technology">Technology</a>
           <a href="/gallery">Gallery</a>
-          <a href="/news">News</a>
           <a href="/contact">Contact</a>
         </nav>
       </header>
