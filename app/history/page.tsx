e01f645cebac76aafae606bb833d932b50a5a02d
Pin銀 <317489986+Pin-Gin@users.noreply.github.com>
2026-09-19 04:32:37 +0900

    Restore World in desktop History navigation

--- app/history/page.tsx
+++ app/history/page.tsx
@@ -16,6 +16,7 @@ export default function HistoryPage() {
           <a href="/technology">Technology</a>
           <a href="/gallery">Gallery</a>
           <a href="/contact">Contact</a>
+          <a href="/world">World</a>
         </nav>
       </header>
 
